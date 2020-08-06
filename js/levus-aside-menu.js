1e1a3661e3792ff69f34daaef00b50a33ca33767
duncan <lvivduncan@gmail.com>
2020-08-06 10:20:46 +0300

    autoclose

--- js/levus-aside-menu.js
+++ js/levus-aside-menu.js
@@ -6,16 +6,22 @@ const lis = menu.querySelectorAll('li');
 lis.forEach(li => {
     if (li.children.length > 1) {
         li.className = 'parent';
-        li.innerHTML += '<span class="close"></span>';
+        li.innerHTML += '<span></span>';
     }
 });
 
 menu.addEventListener('click', e => {
     e.preventDefault();
     if (e.target.tagName === 'SPAN') {
-        // console.log('this span!')
+        // close other .parent ul
+        lis.forEach(li => {
+            if (li.children.length > 1) {
+                li.className = 'parent';
+                // li.innerHTML += '<span class="close"></span>';
+            }
+        });
         // change class to span
-        e.target.classList.toggle('open');
+        // e.target.classList.toggle('open');
         e.target.parentNode.classList.toggle('open');
     }
 });
