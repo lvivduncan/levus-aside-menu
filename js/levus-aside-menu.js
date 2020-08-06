c864e964cba2934c76162baeb808fd383583a85c
duncan <lvivduncan@gmail.com>
2020-08-06 11:15:34 +0300

    transition

--- js/levus-aside-menu.js
+++ js/levus-aside-menu.js
@@ -1,28 +1,25 @@
-// all
-const menu = document.querySelector('.levus-aside-menu');
-// li's
-const lis = menu.querySelectorAll('li');
-// add '.parent' and 'span'
-lis.forEach(li => {
-    if (li.children.length > 1) {
-        li.className = 'parent';
-        li.innerHTML += '<span></span>';
-    }
-});
+{
+    // all
+    const menu = document.querySelector('.levus-aside-menu');
+    // li's
+    const lis = menu.querySelectorAll('li');
+    // add '.parent' and 'span'
+    lis.forEach(li => {
+        if (li.children.length > 1) {
+            li.className = 'parent';
+            li.innerHTML += '<span></span>';
+        }
+    });
 
-menu.addEventListener('click', e => {
-    e.preventDefault();
-    if (e.target.tagName === 'SPAN') {
-        // close other .parent ul
-        lis.forEach(li => {
-            if (li.children.length > 1) {
-                li.className = 'parent';
-                // li.innerHTML += '<span class="close"></span>';
-            }
-        });
-        // change class to span
-        // e.target.classList.toggle('open');
-        e.target.parentNode.classList.toggle('open');
-    }
-});
-// console.log(menu)
+    menu.addEventListener('click', e => {
+        e.preventDefault();
+        if (e.target.tagName === 'SPAN') {
+            // close other .parent ul
+            lis.forEach(li => li.children.length > 1 && (li.className = 'parent'));
+            // setTimeout(() => {
+            // change class to span
+            e.target.parentNode.classList.toggle('open');
+            // }, 500);
+        }
+    });
+}
