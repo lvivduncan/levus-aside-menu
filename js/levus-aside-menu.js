7aaf9b61d3c1609e8b3bd982d061b60e738bc0ef
duncan <lvivduncan@gmail.com>
2020-08-10 10:31:35 +0300

    test...

--- js/levus-aside-menu.js
+++ js/levus-aside-menu.js
@@ -7,17 +7,115 @@
     lis.forEach(li => {
         if (li.children.length > 1) {
             li.className = 'parent';
-            li.innerHTML += '<span></span>';
+            // li.innerHTML += '<span></span>';
         }
     });
 
-    menu.addEventListener('click', e => {
+    const parentsA = document.querySelectorAll('.parent > a');
+
+    parentsA.forEach(parent => {
+        parent.insertAdjacentHTML('afterEnd', '<span></span>');
+    });
+
+
+
+
+
+
+    /* const parents = document.querySelectorAll('.parent');
+    
+    parents.forEach(parent => parent.addEventListener('click', e => {
+        // tmp
         e.preventDefault();
+
+        parents.forEach(parent => {
+            parent.classList.remove('open');
+            parent.lastElementChild.className = '';
+        });
+
+        if (e.target.tagName === 'SPAN') {
+
+            e.target.className = 'open';
+            if (e.target.classList.contains('open')) {
+                e.target.parentNode.classList.toggle('open');
+            }
+
+            // if (flags[i].classList.contains('open')) {
+            //     parent.classList.remove('open');
+            // } else {
+            //     parent.classList.add('open');
+            //     flags[i].classList.add('open');
+            // }
+        }
+    })); */
+
+
+
+
+
+
+    /* const spans = document.querySelectorAll('.parent span');
+
+    spans.forEach(span => span.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        spans.forEach(span => span.parentNode.classList.remove('open'));
+
+        e.stopPropagation();
+        this.parentNode.classList.toggle('open');
+
+        this.classList.toggle('open');
+
+    })); */
+
+
+
+
+
+    const spans = document.querySelectorAll('.parent span');
+
+    spans.forEach(span => {
+        span.addEventListener('click', function() {
+
+            spans.forEach(span => span.classList.remove('open'));
+            // change class
+            this.classList.toggle('open');
+
+        });
+    })
+
+
+
+
+    /* menu.addEventListener('click', function(e) {
+        // tmp
+        e.preventDefault();
+
+
         if (e.target.tagName === 'SPAN') {
             // close other .parent ul
-            lis.forEach(li => li.children.length > 1 && (li.className = 'parent'));
-            // change class to span
-            e.target.parentNode.classList.toggle('open');
+            // lis.forEach(li => li.children.length > 1 && (li.classList.remove('open')));
+
+            spans.forEach(span => span.classList.remove('open'));
+
+            // change class
+            e.target.classList.toggle('open');
+
+            // if (e.target.classList.contains('open')) {
+
+            //     // setTimeout(() => {
+
+            //     e.target.classList.toggle('open');
+            //     // }, 10);
+            // }
+
+            // setTimeout(() => {
+            //     spans.forEach(span => span.classList.remove('open'));
+            // }, 1000);
+
+            // console.log(e.target.parentElement.classList)
+            // console.log(e.target.parentElement.classList.value)
         }
-    });
+    }); */
+
 }
