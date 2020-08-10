16a40700261049236eddd0c9a55b12ef0866f10e
duncan <lvivduncan@gmail.com>
2020-08-10 11:34:30 +0300

    finish

--- js/levus-aside-menu.js
+++ js/levus-aside-menu.js
@@ -3,119 +3,29 @@
     const menu = document.querySelector('.levus-aside-menu');
     // li's
     const lis = menu.querySelectorAll('li');
-    // add '.parent' and 'span'
+    // add '.parent'
     lis.forEach(li => {
         if (li.children.length > 1) {
             li.className = 'parent';
-            // li.innerHTML += '<span></span>';
         }
     });
 
-    const parentsA = document.querySelectorAll('.parent > a');
-
-    parentsA.forEach(parent => {
+    // add 'span'
+    document.querySelectorAll('.parent > a').forEach(parent => {
         parent.insertAdjacentHTML('afterEnd', '<span></span>');
     });
-
-
-
-
-
-
-    /* const parents = document.querySelectorAll('.parent');
-    
-    parents.forEach(parent => parent.addEventListener('click', e => {
-        // tmp
-        e.preventDefault();
-
-        parents.forEach(parent => {
-            parent.classList.remove('open');
-            parent.lastElementChild.className = '';
-        });
-
-        if (e.target.tagName === 'SPAN') {
-
-            e.target.className = 'open';
-            if (e.target.classList.contains('open')) {
-                e.target.parentNode.classList.toggle('open');
-            }
-
-            // if (flags[i].classList.contains('open')) {
-            //     parent.classList.remove('open');
-            // } else {
-            //     parent.classList.add('open');
-            //     flags[i].classList.add('open');
-            // }
-        }
-    })); */
-
-
-
-
-
-
-    /* const spans = document.querySelectorAll('.parent span');
-
-    spans.forEach(span => span.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        spans.forEach(span => span.parentNode.classList.remove('open'));
-
-        e.stopPropagation();
-        this.parentNode.classList.toggle('open');
-
-        this.classList.toggle('open');
-
-    })); */
-
-
-
-
 
     const spans = document.querySelectorAll('.parent span');
 
     spans.forEach(span => {
         span.addEventListener('click', function() {
-
-            spans.forEach(span => span.classList.remove('open'));
+            // check
+            const is_open = this.classList.contains('open');
+            // toggle other 
+            spans.forEach(span => span.classList.toggle('class'));
             // change class
-            this.classList.toggle('open');
-
+            is_open ? this.classList.remove('open') : this.classList.add('open');
         });
     })
-
-
-
-
-    /* menu.addEventListener('click', function(e) {
-        // tmp
-        e.preventDefault();
-
-
-        if (e.target.tagName === 'SPAN') {
-            // close other .parent ul
-            // lis.forEach(li => li.children.length > 1 && (li.classList.remove('open')));
-
-            spans.forEach(span => span.classList.remove('open'));
-
-            // change class
-            e.target.classList.toggle('open');
-
-            // if (e.target.classList.contains('open')) {
-
-            //     // setTimeout(() => {
-
-            //     e.target.classList.toggle('open');
-            //     // }, 10);
-            // }
-
-            // setTimeout(() => {
-            //     spans.forEach(span => span.classList.remove('open'));
-            // }, 1000);
-
-            // console.log(e.target.parentElement.classList)
-            // console.log(e.target.parentElement.classList.value)
-        }
-    }); */
 
 }
