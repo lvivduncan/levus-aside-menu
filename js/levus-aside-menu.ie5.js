bc51acc499da92603e4accd8c36140b9c3883c1a
duncan <lvivduncan@gmail.com>
2020-08-10 12:17:52 +0300

    ie5, min.js

--- js/levus-aside-menu.ie5.js
+++ js/levus-aside-menu.ie5.js
@@ -1,30 +1,29 @@
 // 10-08-2020
-"use strict"; {
+"use strict";
 
-    var menu = document.querySelector('.levus-aside-menu');
+{
+    var menu = document.querySelector('.levus-aside-menu'); // li's
 
-    var lis = menu.querySelectorAll('li');
+    var lis = menu.querySelectorAll('li'); // add '.parent'
 
     lis.forEach(function(li) {
         if (li.children.length > 1) {
             li.className = 'parent';
         }
-    });
+    }); // add 'span'
 
     document.querySelectorAll('.parent > a').forEach(function(parent) {
         parent.insertAdjacentHTML('afterEnd', '<span></span>');
     });
-
     var spans = document.querySelectorAll('.parent span');
-
     spans.forEach(function(span) {
         span.addEventListener('click', function() {
-
-            var is_open = this.classList.contains('open');
+            // check
+            var is_open = this.classList.contains('open'); // toggle other 
 
             spans.forEach(function(span) {
-                return span.classList.toggle('class');
-            });
+                return span.classList.remove('open');
+            }); // change class
 
             if (is_open) {
                 this.classList.remove('open');
